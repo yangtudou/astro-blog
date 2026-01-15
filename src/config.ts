4b363519c8f52700fd74c321e46880bb35dc758b
yang_potato <yangtudou1220@icloud.com>
2026-01-15 23:19:41 +0800

    first add

---
+++ src/config.ts
@@ -0,0 +1,206 @@
+import type { ThemeConfig } from '@/types'
+
+export const themeConfig: ThemeConfig = {
+  // SITE INFORMATION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START
+  site: {
+    // site title
+    title: '洋san芋suo',
+    // site subtitle
+    subtitle: '时间就是最巨大的谎',
+    // site description
+    description: '学着数字化自己的生活。',
+    // use i18n title/subtitle/description from src/i18n/ui.ts instead of static ones above
+    i18nTitle: false, // true | false
+    // author name
+    author: 'yangtudou',
+    // site url
+    url: 'https://blog.y3-3am.top',
+    // base path
+    // root directory for all pages and assets
+    base: '/', // e.g., '/blog', '/docs'
+    // favicon url
+    // recommended formats: svg, png or ico
+    favicon: '/icons/favicon.svg', // or https://example.com/favicon.svg
+  },
+  // SITE INFORMATION >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END
+
+  // COLOR SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START
+  color: {
+    // default theme mode
+    mode: 'light', // light | dark | auto
+    light: {
+      // primary color
+      // used for title, hover, etc
+      // oklch color picker: https://oklch.com/
+      primary: 'oklch(25% 0.005 298)',
+      // secondary color
+      // used for post text
+      secondary: 'oklch(40% 0.005 298)',
+      // background color
+      background: 'oklch(96% 0.005 298)',
+      // highlight color
+      // used for navbar, selected text, etc
+      highlight: 'oklch(0.93 0.195089 103.2532 / 0.5)', // rgba(255,235,0,0.5)
+    },
+    dark: {
+      // primary color
+      primary: 'oklch(92% 0.005 298)',
+      // secondary color
+      secondary: 'oklch(77% 0.005 298)',
+      // background color
+      background: 'oklch(22% 0.005 298)',
+      // highlight color
+      highlight: 'oklch(0.93 0.195089 103.2532 / 0.2)', // rgba(255,235,0,0.2)
+    },
+  },
+  // COLOR SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END
+
+  // GLOBAL SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START
+  global: {
+    // default language
+    locale: 'zh', // de | en | es | fr | ja | ko | pl | pt | ru | zh | zh-tw
+    // more languages
+    // not fill in the locale code above again, can be an empty array []
+    moreLocales: [], // ['de', 'en', 'es', 'fr', 'ja', 'ko', 'pl', 'pt', 'ru', 'zh', 'zh-tw']
+    // post font style
+    fontStyle: 'sans', // sans | serif
+    // post date format
+    dateFormat: 'YYYY-MM-DD', // YYYY-MM-DD | MM-DD-YYYY | DD-MM-YYYY | MMM D YYYY | D MMM YYYY
+    // enable table of contents
+    toc: true, // true | false
+    // enable katex math rendering
+    katex: true, // true | false
+    // reduce motion
+    reduceMotion: false, // true | false
+  },
+  // GLOBAL SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END
+
+  // COMMENT SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START
+  comment: {
+    // enable comment system
+    enabled: false, // true | false
+    // giscus
+    // https://giscus.app/
+    giscus: {
+      repo: '',
+      repoId: '',
+      category: '',
+      categoryId: '',
+      mapping: 'pathname',
+      strict: '0',
+      reactionsEnabled: '1',
+      emitMetadata: '0',
+      inputPosition: 'bottom',
+    },
+    // twikoo
+    // https://twikoo.js.org/
+    twikoo: {
+      envId: '',
+      // version: frontend version can be changed in package.json
+    },
+    // waline
+    // https://waline.js.org/en/
+    waline: {
+      // server url
+      serverURL: 'https://retypeset-comment.radishzz.cc',
+      // emoji url
+      emoji: [
+        'https://unpkg.com/@waline/emojis@1.2.0/tw-emoji',
+        // 'https://unpkg.com/@waline/emojis@1.2.0/bmoji',
+        // more emojis: https://waline.js.org/en/guide/features/emoji.html
+      ],
+      // gif search
+      search: false, // true | false
+      // image uploader
+      imageUploader: false, // true | false
+    },
+  },
+  // COMMENT SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END
+
+  // SEO SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START
+  seo: {
+    // @twitter ID
+    twitterID: '',
+    // site verification
+    verification: {
+      // google search console
+      // https://search.google.com/search-console
+      google: '',
+      // bing webmaster tools
+      // https://www.bing.com/webmasters
+      bing: '',
+      // yandex webmaster
+      // https://webmaster.yandex.com
+      yandex: '',
+      // baidu search
+      // https://ziyuan.baidu.com
+      baidu: '',
+    },
+    // google analytics
+    // https://analytics.google.com
+    googleAnalyticsID: '',
+    // umami analytics
+    // https://cloud.umami.is
+    umamiAnalyticsID: '',
+    // follow verification
+    // https://follow.is/
+    follow: {
+      // feed ID
+      feedID: '',
+      // user ID
+      userID: '',
+    },
+    // apiflash access key
+    // generate website screenshots for open graph images
+    // get your access key at: https://apiflash.com/
+    apiflashKey: '',
+  },
+  // SEO SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END
+
+  // FOOTER SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START
+  footer: {
+    // social links
+    links: [
+      // {
+      //   name: 'RSS',
+      //   url: '/atom.xml', // or /rss.xml
+      // },
+      {
+        name: 'GitHub',
+        url: 'https://github.com/yangtudou',
+      },
+      {
+        name: 'Email',
+        url: '676683117@rqq.com',
+      },
+      // {
+      //   name: 'X',
+      //   url: 'https://x.com/radishzz_',
+      // },
+    ],
+    // year of website start
+    startYear: 2020,
+  },
+  // FOOTER SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END
+
+  // PRELOAD SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> START
+  preload: {
+    // image hosting url
+    // optimize remote images and generate low-quality placeholders
+    imageHostURL: '',
+    // custom google analytics js
+    // for users who proxy tracking scripts to a custom domain
+    // see https://gist.github.com/xiaopc/0602f06ca465d76bd9efd3dda9393738
+    customGoogleAnalyticsJS: '',
+    // custom umami analytics js
+    // for users who self-deploy umami or proxy tracking scripts to a custom domain
+    // see https://umami.is/docs/bypass-ad-blockers
+    customUmamiAnalyticsJS: '',
+  },
+  // PRELOAD SETTINGS >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> END
+}
+
+export const base = themeConfig.site.base === '/' ? '' : themeConfig.site.base.replace(/\/$/, '')
+export const defaultLocale = themeConfig.global.locale
+export const moreLocales = themeConfig.global.moreLocales
+export const allLocales = [defaultLocale, ...moreLocales]
---
+++ src/content/posts/Hello Astro.md
@@ -0,0 +1,9 @@
+---
+title: Hello Astro
+published: 2026-01-15
+draft: true
+# lang: 'zh'
+description: 这是我转 Astro 搭建博客系统后的第一篇。
+---
+
+
