0ca7858a723ee7db1d0250d505af9cc3ee239888
yangsanyu <676683117@qq.com>
2026-01-23 17:08:07 +0800

    大框架搭建

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,14 @@ import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
+    markdown: {
+        shikiConfig: {
+            themes: {
+                light: 'vitesse-light',
+                dark: 'vitesse-dark',
+            },
+        }
+    },
     integrations: [
         UnoCSS({
             injectReset: true
