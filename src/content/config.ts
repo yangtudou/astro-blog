afeb0a5b806aeaff378abe37fdc84ea5034d2156
yang_potato <yangtudou1220@icloud.com>
2026-01-31 22:43:42 +0800

    增加 talk

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,6 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const postCollection = defineCollection({
-  // 移除 type: 'content'，改用 loader
   loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./posts" }),
   schema: z.object({
     title: z.string(),
@@ -13,6 +12,21 @@ const postCollection = defineCollection({
   }),
 });
 
+const talkCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./talks" }),
+  // 修改这里：增加 { image } 参数
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    tag: z.string().optional(),
+    // 修改这里：使用 image() 校验器
+    cover: image(), 
+    publishDate: z.date(),
+    excerpt: z.string().optional(),
+    draft: z.boolean().optional().default(false),
+  }),
+});
+
 export const collections = {
   'post': postCollection,
+  'talk': talkCollection,
 };
